f7f00e1b5b41c28a247dcceaa4f3e02bc1679848
Shay <luan.yq.shay@gmail.com>
2026-04-28 11:36:01 -0400

    foundational godot

---
+++ Foundations/foundations/Scenes/Ocean/Ocean.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+@onready var plane: Sprite2D = $Plane
+@onready var helicopter: Sprite2D = $Helicopter
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	plane.move_local_x(250.0 * delta)
+	helicopter.move_local_x(150.0 * delta)
+	helicopter.rotate(2 * delta)
+	
+	if Input.is_action_pressed("ui_left"):
+		plane.rotate(-1.5 * delta)
+	if Input.is_action_pressed("ui_right"):
+		plane.rotate(1.5 * delta)
