3f92a0765b0075f65215ee95f6d6c3864eac5c8b
Shay <luan.yq.shay@gmail.com>
2026-04-30 12:27:33 -0400

    getters and setters syntax

--- Foundations/foundations/BeyondBasics/Character.gd
+++ Foundations/foundations/BeyondBasics/Character.gd
@@ -1,16 +1,49 @@
 class_name Character
 
-var health: int = 50
-var name: String = "Riley"
-var weapon: String = "Whip"
+var _health: int = 50
+
+#var health: int:
+	#get:
+		#print("get health")
+		#return _health
+	#set(value):
+		#print("set health")
+		#_health = clampi(value, 0, 300)
+
+var health: int: 
+	get = get_health, set = set_health
+	
+var weapon: String = "CURRENTLY NO WEAPON":
+	get:
+		return weapon
+	set(value):
+		weapon = value
+
+var name: String = "CURRENTLY NO NAME":
+	get:
+		return name
+	set(value):
+		name = value
+
+
 
 func _init(p_health: int, p_name: String, p_weapon: String) -> void:
-	health = p_health
+	_health = p_health
 	name = p_name
 	weapon = p_weapon
 
 func print_health() -> void:
-	print("Health: %d" % health)
+	print("Health: %d" % _health)
 
 func say_info() -> void: 
-	print("%s wields a %s\n%s has %d health" % [name, weapon, name, health])
+	print("%s wields a %s\n%s has %d health" % [name, weapon, name, _health])
+	
+func get_health() -> int:
+	print("get_health() invoked")
+	return _health
+	
+func set_health(new_health: int) -> void:
+	print("set_health() invoked")
+	if new_health >= 300: health = 300
+	elif new_health <= 0: health = 0
+	
--- Foundations/foundations/BeyondBasics/Runner.gd
+++ Foundations/foundations/BeyondBasics/Runner.gd
@@ -4,7 +4,27 @@ extends EditorScript
 
 # Called when the script is executed (using File -> Run in Script Editor).
 func _run() -> void:
-	print("I am alive")
-	
+	#print("I am alive")
+	#
+	#var riley: Character = Character.new(250, "Riley", "Whip")
+	#riley.say_info()
+	#print("Riley takes 20 damage. ")
+	#riley._health -= 20
+	#riley.print_health()
+	#
+	#print("Using health getter: ", riley.get_health())
+	#riley.set_health(400)
+	#riley.print_health()
+	#riley.set_health(234)
+	#riley.say_info()
+
 	var riley: Character = Character.new(250, "Riley", "Whip")
-	riley.say_info()
+	riley.health = 2000
+	print(riley.health)
+	
+	var kenzie: Character = Character.new(180, "Kenzie", "Dagger")
+	kenzie.say_info()
+	
+	
+	
+	
