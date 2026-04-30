93c9fa6d69da9b0c21eb89512b1ebf686eadb304
Shay <luan.yq.shay@gmail.com>
2026-04-30 11:59:16 -0400

    constructors and methods

--- Foundations/foundations/BeyondBasics/Character.gd
+++ Foundations/foundations/BeyondBasics/Character.gd
@@ -1,6 +1,16 @@
 class_name Character
 
 var health: int = 50
+var name: String = "Riley"
+var weapon: String = "Whip"
+
+func _init(p_health: int, p_name: String, p_weapon: String) -> void:
+	health = p_health
+	name = p_name
+	weapon = p_weapon
 
 func print_health() -> void:
 	print("Health: %d" % health)
+
+func say_info() -> void: 
+	print("%s wields a %s\n%s has %d health" % [name, weapon, name, health])
--- Foundations/foundations/BeyondBasics/Runner.gd
+++ Foundations/foundations/BeyondBasics/Runner.gd
@@ -5,13 +5,6 @@ extends EditorScript
 # Called when the script is executed (using File -> Run in Script Editor).
 func _run() -> void:
 	print("I am alive")
-	var aragorn: Character = Character.new()
-	aragorn.print_health()
-	aragorn.health = 100
-	aragorn.print_health()
 	
-	var riley: Character = Character.new()
-	riley.print_health()
-	print("Setting riley's health to 200... ")
-	riley.health = 200
-	riley.print_health()
+	var riley: Character = Character.new(250, "Riley", "Whip")
+	riley.say_info()
