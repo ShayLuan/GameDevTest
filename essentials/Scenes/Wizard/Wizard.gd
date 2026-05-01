31327bacd932c9e2e5bfb29ef0f72827133063e8
Shay <luan.yq.shay@gmail.com>
2026-05-01 14:40:23 -0400

    signals part 1

--- essentials/Scenes/Wizard/Wizard.gd
+++ essentials/Scenes/Wizard/Wizard.gd
@@ -1,13 +1,11 @@
 class_name Wizard
 
 extends Node2D
-
+@onready var reveal_timer: Timer = $RevealTimer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	hide()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_reveal_timer_timeout() -> void:
+	show() 
