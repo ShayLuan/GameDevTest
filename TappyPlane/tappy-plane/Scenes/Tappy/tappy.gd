e5bf9ebd12691160db9cc7dd757b3c01ab9da262
Shay <luan.yq.shay@gmail.com>
2026-05-17 11:11:10 -0400

    dying mechanic

--- TappyPlane/tappy-plane/Scenes/Tappy/tappy.gd
+++ TappyPlane/tappy-plane/Scenes/Tappy/tappy.gd
@@ -2,6 +2,8 @@ extends CharacterBody2D
 
 const JUMP_POWER: float = -350.0
 
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
 var _gravity: float = ProjectSettings.get("physics/2d/default_gravity")
 var _jumped: bool = false
 
@@ -9,6 +11,10 @@ func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("power"):
 		_jumped = true
 
+func die() -> void:
+	set_physics_process(false)
+	animated_sprite_2d.stop()
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
@@ -21,3 +27,9 @@ func _physics_process(delta: float) -> void:
 		#velocity.y = JUMP_POWER
 	
 	move_and_slide()
+	
+	if is_on_floor():
+		die()
+		
+	if is_on_ceiling():
+		die()
