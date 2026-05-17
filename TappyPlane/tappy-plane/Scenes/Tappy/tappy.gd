288f554533103683435d43a6074033bd3437ad91
Shay <luan.yq.shay@gmail.com>
2026-05-17 09:23:01 -0400

    plane move with gravity

---
+++ TappyPlane/tappy-plane/Scenes/Tappy/tappy.gd
@@ -0,0 +1,13 @@
+extends CharacterBody2D
+
+var _gravity: float = ProjectSettings.get("physics/2d/default_gravity")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta: float) -> void:
+	velocity.y += _gravity * delta
+	move_and_slide()
