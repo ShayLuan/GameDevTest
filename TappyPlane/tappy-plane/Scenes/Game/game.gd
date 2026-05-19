14b5d7327e5a53f63434666e08e5bf2ea714d3f3
Shay <luan.yq.shay@gmail.com>
2026-05-19 13:31:45 -0400

    randomized spawn sequence

---
+++ TappyPlane/tappy-plane/Scenes/Game/game.gd
@@ -0,0 +1,23 @@
+extends Node
+const PIPES = preload("res://Scenes/Pipes/Pipes.tscn")
+
+@onready var pipes_holder: Node = $PipesHolder
+@onready var upper_spawn: Marker2D = $UpperSpawn
+@onready var lower_spawn: Marker2D = $LowerSpawn
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	spawn_pipes()
+
+func spawn_pipes() -> void:
+	var new_pipes: Pipes = PIPES.instantiate()
+	var y_position: float = randf_range(
+		upper_spawn.position.y, lower_spawn.position.y
+	)
+	new_pipes.position = Vector2(
+		upper_spawn.position.x, y_position
+	)
+	pipes_holder.add_child(new_pipes)
+	
+func _on_spawn_timer_timeout() -> void:
+	spawn_pipes()
