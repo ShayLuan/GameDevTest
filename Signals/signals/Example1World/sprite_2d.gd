2eb183dcb3d285f635ebdf136351f12c53f90359
Shay <luan.yq.shay@gmail.com>
2026-05-20 16:08:56 -0400

    button signal to stop

--- Signals/signals/Example1World/sprite_2d.gd
+++ Signals/signals/Example1World/sprite_2d.gd
@@ -4,11 +4,20 @@ var speed = 400
 var angular_speed = PI
 var toggled_on = true
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+## Called when the node enters the scene tree for the first time.
+#func _ready() -> void:
+	#pass # Replace with function body.
 
+func toggle() -> void:
+	toggled_on = !toggled_on
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if toggled_on:
+		rotation += angular_speed * delta
+		var velocity = Vector2.UP.rotated(rotation) * speed
+		position += velocity * delta
+
+
+func _on_button_pressed() -> void:
+	toggle()
