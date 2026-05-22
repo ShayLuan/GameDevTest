fb59f7dafa95f15dee757205c4c9c65e4c8ba12c
Shay <luan.yq.shay@gmail.com>
2026-05-22 11:41:43 -0400

    fixed character physics movement + collision

---
+++ Signals/signals/Example2World/Player/character.gd
@@ -0,0 +1,28 @@
+extends CharacterBody2D
+
+@onready var player: Sprite2D = $Player
+
+
+const MOVE_SPEED: float = 300.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta: float) -> void:
+	var direction_x := Input.get_axis("ui_left", "ui_right")
+	var direction_y := Input.get_axis("ui_up", "ui_down")
+	
+	# 2. Assign the movement to the built-in velocity variable
+	if direction_x:
+		velocity.x = direction_x * MOVE_SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, MOVE_SPEED)
+		
+	if direction_y:
+		velocity.y = direction_y * MOVE_SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, MOVE_SPEED)
+	move_and_slide()
--- Signals/signals/Example2World/example_2_world.gd
+++ Signals/signals/Example2World/example_2_world.gd
@@ -7,17 +7,18 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	pass
 
 
 func _on_plate_1_body_entered(body: Node2D) -> void:
+	print("body entered")
 	print(body)
-
 
 func _on_plate_2_body_entered(body: Node2D) -> void:
+	print("body entered")
 	print(body)
 
-
 func _on_plate_3_body_entered(body: Node2D) -> void:
+	print("body entered")
 	print(body)
