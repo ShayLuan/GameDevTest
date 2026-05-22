bf01f46000a816dabacdb4c564a2a1be3dd4c9d0
Shay <luan.yq.shay@gmail.com>
2026-05-22 12:50:34 -0400

    background changing colors

--- Signals/signals/Example2World/example_2_world.gd
+++ Signals/signals/Example2World/example_2_world.gd
@@ -1,9 +1,15 @@
 extends Node2D
+@onready var background: Sprite2D = $Background
+@onready var background_2: Sprite2D = $Background2
+@onready var background_3: Sprite2D = $Background3
+@onready var background_4: Sprite2D = $Background4
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	background_2.visible = false
+	background_3.visible = false
+	background_4.visible = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -12,13 +18,17 @@ func _physics_process(delta: float) -> void:
 
 
 func _on_plate_1_body_entered(body: Node2D) -> void:
-	print("body entered")
 	print(body)
+	print("Entered Pink")
+	background_2.visible = true
+
 
 func _on_plate_2_body_entered(body: Node2D) -> void:
-	print("body entered")
 	print(body)
+	print("Entered Yellow")
+	background_3.visible = true
 
 func _on_plate_3_body_entered(body: Node2D) -> void:
-	print("body entered")
 	print(body)
+	print("Entered Blue")
+	background_4.visible = true
