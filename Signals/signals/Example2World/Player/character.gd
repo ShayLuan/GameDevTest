b040bac2a8b8fc151c31583dc8b2af81cdc39fb4
Shay <luan.yq.shay@gmail.com>
2026-05-24 10:32:58 -0400

    fixed gravity, removed y axis

--- Signals/signals/Example2World/Player/character.gd
+++ Signals/signals/Example2World/Player/character.gd
@@ -12,27 +12,22 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
+			
+	########################################################
+	##################### GRAVITY ##########################
+	########################################################
+	if not is_on_floor():
+		velocity.y += GRAVITY * delta
+		
 	########################################################
 	################# DIRECTIONAL MOVEMENT #################
 	########################################################
 	var direction_x := Input.get_axis("ui_left", "ui_right")
-	var direction_y := Input.get_axis("ui_up", "ui_down")
 	
 	# 2. Assign the movement to the built-in velocity variable
 	if direction_x:
 		velocity.x = direction_x * MOVE_SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, MOVE_SPEED)
-		
-	if direction_y:
-		velocity.y = direction_y * MOVE_SPEED
-	else:
-		velocity.y = move_toward(velocity.y, 0, MOVE_SPEED)
-		
-	########################################################
-	##################### GRAVITY ##########################
-	########################################################
-	if not is_on_floor():
-		velocity.y += GRAVITY * delta
 		
 	move_and_slide()
