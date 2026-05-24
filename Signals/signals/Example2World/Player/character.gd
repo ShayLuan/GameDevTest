c5900297b33e61bf6488f7b7927859aa752e713e
Shay <luan.yq.shay@gmail.com>
2026-05-24 10:15:33 -0400

    gravity added

--- Signals/signals/Example2World/Player/character.gd
+++ Signals/signals/Example2World/Player/character.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 @onready var player: Sprite2D = $Player
 
-
+var GRAVITY: float = ProjectSettings.get_setting("physics/2d/default_gravity")
 const MOVE_SPEED: float = 300.0
 
 # Called when the node enters the scene tree for the first time.
@@ -12,6 +12,9 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
+	########################################################
+	################# DIRECTIONAL MOVEMENT #################
+	########################################################
 	var direction_x := Input.get_axis("ui_left", "ui_right")
 	var direction_y := Input.get_axis("ui_up", "ui_down")
 	
@@ -25,4 +28,11 @@ func _physics_process(delta: float) -> void:
 		velocity.y = direction_y * MOVE_SPEED
 	else:
 		velocity.y = move_toward(velocity.y, 0, MOVE_SPEED)
+		
+	########################################################
+	##################### GRAVITY ##########################
+	########################################################
+	if not is_on_floor():
+		velocity.y += GRAVITY * delta
+		
 	move_and_slide()
